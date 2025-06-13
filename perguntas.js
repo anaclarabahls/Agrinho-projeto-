8b444dcf9ec6881c0218eb167c0af29369f0c227
anaclarabahls <160857705+anaclarabahls@users.noreply.github.com>
2025-06-13 08:20:19 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Cultura',
+    'Como é a cultura?',
+    'rica e diversa, é o resultado da mistura de costumes e tradições de diversos grupos étnicos que povoaram o estado.'
+)
+
+criaCartao(
+    'Culinária',
+    'Como é a culinária?',
+    'A culinária paranaense, com influência das tradições indígenas, portuguesas e de imigrantes, é rica em sabores e técnicas de preparo. São exemplos da diversidade e do potencial gastronômico do estado. '
+)
+
+criaCartao(
+    'Turismo',
+    'Tem muitos pontos turisticos?',
+    'Sim, Cataratas do Iguaçu, a Usina Hidrelétrica de Itaipu, o Parque das Aves, o Jardim Botânico de Curitiba e a Ilha do Mel, entre outros.'
+)
+
+criaCartao(
+    'Gírias',
+    'Existem muitas gírias locais?',
+    'Sim,  mais comuns incluem "piá" (menino, jovem), "guria" (menina, jovem), "baita" (grande, impressionante), "treta" (confusão, problema) e "capaz" (usado para surpresa ou negação)'
+)
+
+criaCartao(
+    'Clima',
+    'E o clima como é?',
+    'O clima predominante no Paraná é subtropical úmido, com temperaturas amenas e chuvas bem distribuídas ao longo do ano. '
+)
+
+
+criaCartao(
+    'Animais',
+    'Existem animais nativos?',
+    'Sim, por exemplo, Gralha-azul, a Raposa-dos-Pampas, a Jaguatirica, o Guaxinim, o Lobo-guará, o Papagaio-da-cara-roxa e o Guará. '
+)
