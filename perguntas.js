171642a43a06b5a6164f01a5b6aefce4d8f34226
anaclarabahls <160857705+anaclarabahls@users.noreply.github.com>
2025-06-13 08:28:15 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
     'Benefício de se morar no campo',
     'Mais qualidade de vida',
-    'O ritmo de vida costuma ser mais tranquilo, com menos poluição sonora e do ar, o que contribui para a saúde física e mental. Há mais oportunidades para atividades ao ar livre e contato com a natureza, reduzindo o estresse.' 
+    'O ritmo de vida costuma ser mais tranquilo, com menos poluição sonora e do ar, o que contribui para a saúde física e mental.'
 )
 
 criaCartao(
     'Benfícios de se morar na cidade',
     'Acesso facilitado a serviços e conveniências',
-    'Nas cidades, geralmente encontramos uma grande variedade de serviços como hospitais, escolas, bancos, supermercados e farmácias, muitas vezes com horários flexíveis e localizações convenientes. O transporte público costuma ser mais abrangente, facilitando a locomoção.'
+    'Nas cidades, geralmente encontramos uma grande variedade de serviços como hospitais, escolas, muitas vezes com horários flexíveis e localizações convenientes. '
 )
 
 criaCartao(
@@ -19,18 +19,20 @@ criaCartao(
 criaCartao(
     'Malefícios de se morar no campo',
     'Acesso limitado a serviços e conveniências',
-    'No campo, pode haver uma menor disponibilidade de serviços básicos como hospitais especializados, grandes supermercados, agências bancárias e opções de lazer e cultura. A distância para centros urbanos pode dificultar o acesso a esses serviços.'
+    'No campo, pode haver uma menor disponibilidade de serviços básicos como hospitais especializados, grandes supermercados, agências bancárias e opções de lazer e cultura.'
 )
 
 criaCartao(
     'Malefícios de se morar na cidade',
     'Alto custo de vida',
-    'Em geral, o custo de vida nas cidades, especialmente nas grandes metrópoles, tende a ser mais elevado. Isso inclui moradia (aluguel ou financiamento), transporte, alimentação e serviços, o que pode impactar significativamente o orçamento.'
+    'Em geral, o custo de vida nas cidades, especialmente nas grandes metrópoles, tende a ser mais elevado.'
+
 )
 
 
 criaCartao(
     'Curiosidade',
     'A "selva de pedra" também tem seus refúgios verdes',
-    'Apesar da grande quantidade de prédios e concreto, muitas cidades investem em espaços verdes como parques, praças e jardins. Esses oásis urbanos oferecem um respiro da agitação, proporcionando áreas para lazer, exercícios e contato com a natureza no meio da vida urbana.'
+    'Apesar da grande quantidade de prédios e concreto, muitas cidades investem em espaços verdes como parques, praças e jardins.'
+
 )
