00d2623d51eb20fcd5addfdf5fc053c613715516
anaclarabahls <160857705+anaclarabahls@users.noreply.github.com>
2025-06-13 08:37:03 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -11,7 +11,7 @@ criaCartao(
 )
 
 criaCartao(
-    'Curiosidade'
+    'Curiosidade',
     'Cidade paranaense conhecida como a Capital Ecológica do Brasil.',
     'Curitiba, a capital do Paraná, é famosa por esse título! A cidade se destaca por seu planejamento urbano inovador e por ser um exemplo global em sustentabilidade. '
    
