34e0459bbad144d34416ab24fc7afc64b95f34e1
anaclarabahls <160857705+anaclarabahls@users.noreply.github.com>
2025-06-13 08:38:33 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,12 +1,12 @@
 criaCartao(
     'Benefício de se morar no campo',
-    'Mais qualidade de vida',
+    'Mais qualidade de vida.',
     'O ritmo de vida costuma ser mais tranquilo, com menos poluição sonora e do ar, o que contribui para a saúde física e mental.'
 )
 
 criaCartao(
     'Benfícios de se morar na cidade',
-    'Acesso facilitado a serviços e conveniências',
+    'Acesso facilitado a serviços e conveniências.',
     'Nas cidades, geralmente encontramos uma grande variedade de serviços como hospitais, escolas, muitas vezes com horários flexíveis e localizações convenientes. '
 )
 
@@ -19,13 +19,13 @@ criaCartao(
 
 criaCartao(
     'Malefícios de se morar no campo',
-    'Acesso limitado a serviços e conveniências',
+    'Acesso limitado a serviços e conveniências.',
     'No campo, pode haver uma menor disponibilidade de serviços básicos como hospitais especializados, grandes supermercados, agências bancárias e opções de lazer e cultura.'
 )
 
 criaCartao(
     'Malefícios de se morar na cidade',
-    'Alto custo de vida',
+    'Alto custo de vida.',
     'Em geral, o custo de vida nas cidades, especialmente nas grandes metrópoles, tende a ser mais elevado.'
 
 )
@@ -33,7 +33,7 @@ criaCartao(
 
 criaCartao(
     'Curiosidade',
-    'A "selva de pedra" também tem seus refúgios verdes',
+    'A "selva de pedra" também tem seus refúgios verdes.',
     'Apesar da grande quantidade de prédios e concreto, muitas cidades investem em espaços verdes como parques, praças e jardins.'
 
 )
